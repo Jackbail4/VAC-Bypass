3d6dead85fb5bc30f154f6887bc57f12cdb61d57
Jackbail4 <64487175+Jackbail4@users.noreply.github.com>
2022-01-30 13:03:31 +0000

    Update Hooks.h

--- Vac bypass/Hooks.h
+++ Vac bypass/Hooks.h
@@ -1,94 +1,93 @@
-#pragma once
 #include <Windows.h>
 #include <TlHelp32.h>
-#include <ntstatus.h>
+#include <string>
 
 #include "detours.h"
 #pragma comment(lib, "detours.lib")
 
 //VirtualQuery
 typedef SIZE_T(WINAPI* tVirtualQuery)(LPCVOID lpAdress, PMEMORY_BASIC_INFORMATION lpBuffer, SIZE_T dwLength);
-tVirtualQuery pVirtualQuery;
+inline tVirtualQuery pVirtualQuery;
 
-SIZE_T WINAPI hkVirtualQuery(LPCVOID lpAdress, PMEMORY_BASIC_INFORMATION lpBuffer, SIZE_T dwLength);
+static SIZE_T WINAPI hkVirtualQuery(LPCVOID lpAdress, PMEMORY_BASIC_INFORMATION lpBuffer, SIZE_T dwLength);
 
 //VirtualQueryEx
 typedef SIZE_T(WINAPI* tVirtualQueryEx)(HANDLE hProcess, LPCVOID lpAdress, PMEMORY_BASIC_INFORMATION lpBuffer, SIZE_T dwLength);
-tVirtualQueryEx pVirtualQueryEx;
+inline tVirtualQueryEx pVirtualQueryEx;
 
-SIZE_T WINAPI hkVirtualQueryEx(HANDLE hProcess, LPCVOID lpAdress, PMEMORY_BASIC_INFORMATION lpBuffer, SIZE_T dwLength);
+static SIZE_T WINAPI hkVirtualQueryEx(HANDLE hProcess, LPCVOID lpAdress, PMEMORY_BASIC_INFORMATION lpBuffer, SIZE_T dwLength);
 
 //GetProcessId
 typedef DWORD(WINAPI* tGetProcessId)(HANDLE hProcess);
-tGetProcessId pGetProcessId;
+inline tGetProcessId pGetProcessId;
 
-DWORD WINAPI hkGetProcessId(HANDLE hProcess);
+static DWORD WINAPI hkGetProcessId(HANDLE hProcess);
 
 //Process32NextW
 typedef BOOL(WINAPI* tProcess32NextW)(HANDLE hSnapShot, LPPROCESSENTRY32W lppe);
-tProcess32NextW pProcess32NextW;
+inline tProcess32NextW pProcess32NextW;
 
-BOOL WINAPI hkProcess32NextW(HANDLE hSnapShot, LPPROCESSENTRY32W lppe);
+static BOOL WINAPI hkProcess32NextW(HANDLE hSnapShot, LPPROCESSENTRY32W lppe);
 
 //Module32NextW
 
 typedef BOOL(WINAPI* tModule32NextW)(HANDLE hSnapShot, LPMODULEENTRY32W lppe);
-tModule32NextW pModule32NextW;
+inline tModule32NextW pModule32NextW;
 
-BOOL WINAPI hkModule32NextW(HANDLE hSnapShot, LPMODULEENTRY32W lppe);
+static BOOL WINAPI hkModule32NextW(HANDLE hSnapShot, LPMODULEENTRY32W lppe);
 
 //ReadProcessMemory
 typedef BOOL(WINAPI* tReadProcessMemory)(HANDLE hProcess, LPCVOID lpBaseAdress, LPVOID lpBuffer, SIZE_T nSize, SIZE_T* lpNumberOfBytesRead);
-tReadProcessMemory pReadProcessMemory;
+inline tReadProcessMemory pReadProcessMemory;
 
-BOOL WINAPI hkReadProcessMemory(HANDLE hProcess, LPCVOID lpBaseAdress, LPVOID lpBuffer, SIZE_T nSize, SIZE_T* lpNumberOfBytesRead);
+static BOOL WINAPI hkReadProcessMemory(HANDLE hProcess, LPCVOID lpBaseAdress, LPVOID lpBuffer, SIZE_T nSize, SIZE_T* lpNumberOfBytesRead);
 
 //OpenProcessToken
 typedef BOOL(WINAPI* tOpenProcessToken)(HANDLE ProcessHandle, DWORD DesiredAccess, PHANDLE TokenHandle);
-tOpenProcessToken pOpenProcessToken;
+inline tOpenProcessToken pOpenProcessToken;
 
-BOOL WINAPI hkOpenProcessToken(HANDLE ProcessHandle, DWORD DesiredAccess, PHANDLE TokenHandle);
+static BOOL WINAPI hkOpenProcessToken(HANDLE ProcessHandle, DWORD DesiredAccess, PHANDLE TokenHandle);
 
 //GetSystemInfo
 typedef void(WINAPI* tGetSystemInfo)(LPSYSTEM_INFO lpSystemInfo);
-tGetSystemInfo pGetSystemInfo;
+inline tGetSystemInfo pGetSystemInfo;
 
-void WINAPI hkGetSystemInfo(LPSYSTEM_INFO lpSystemInfo);
+static void WINAPI hkGetSystemInfo(LPSYSTEM_INFO lpSystemInfo);
 
 //GetCurrentProcess
 typedef HANDLE(WINAPI* tGetCurrentProcess)();
-tGetCurrentProcess pGetCurrentProcess;
+inline tGetCurrentProcess pGetCurrentProcess;
 
-HANDLE WINAPI hkGetCurrentProcess();
+static HANDLE WINAPI hkGetCurrentProcess();
 
 //QueryDosDeviceW
 typedef DWORD(WINAPI* tQueryDosDeviceW)(LPCWSTR lpDeviceName, LPWSTR lpTargetPath, DWORD ucchMax);
-tQueryDosDeviceW pQueryDosDeviceW;
+inline tQueryDosDeviceW pQueryDosDeviceW;
 
-DWORD WINAPI hkQueryDosDeviceW(LPCWSTR lpDeviceName, LPWSTR lpTargetPath, DWORD ucchMax);
+static DWORD WINAPI hkQueryDosDeviceW(LPCWSTR lpDeviceName, LPWSTR lpTargetPath, DWORD ucchMax);
 
 //GetFileInfomationByHandle
 typedef BOOL(WINAPI* tGetFileInformationByHandle)(HANDLE hFile, LPBY_HANDLE_FILE_INFORMATION lpFileInformation);
-tGetFileInformationByHandle pGetFileInformationByHandle;
+inline tGetFileInformationByHandle pGetFileInformationByHandle;
 
-BOOL _stdcall hkGetFileInformationByHandle(HANDLE hFile, LPBY_HANDLE_FILE_INFORMATION lpFileInformation);
+static BOOL _stdcall hkGetFileInformationByHandle(HANDLE hFile, LPBY_HANDLE_FILE_INFORMATION lpFileInformation);
 
 //GetWindowTextA
 typedef int (WINAPI* tGetWindowTextA)(HWND hWnd, LPSTR lpString, int nMaxCount);
-tGetWindowTextA pGetWindowTextA;
+inline tGetWindowTextA pGetWindowTextA;
 
-int WINAPI hkGetWindowTextA(HWND hwnd, LPSTR lpString, int nMaxCount);
+static int WINAPI hkGetWindowTextA(HWND hwnd, LPSTR lpString, int nMaxCount);
 
 //BitBlt
 typedef BOOL (WINAPI* tBitBlt)(HDC hdc, int x, int y, int cx, int cy, HDC hdcSrc, int x1, int y1, DWORD rop);
-tBitBlt pBitBlt;
+inline tBitBlt pBitBlt;
 
-BOOL WINAPI hkBitBlt(HDC hdc, int x, int y, int cx, int cy, HDC hdcSrc, int x1, int y1, DWORD rop);
+static BOOL WINAPI hkBitBlt(HDC hdc, int x, int y, int cx, int cy, HDC hdcSrc, int x1, int y1, DWORD rop);
 
 //GetProcAddress
 typedef FARPROC(WINAPI* tGetProcAddress)(HMODULE hModule, LPCSTR lpProcName);
-tGetProcAddress pGetProcAddress;
+inline tGetProcAddress pGetProcAddress;
 
-FARPROC __stdcall hkGetProcAddress(HMODULE hModule, LPCSTR lpProcName);
+static FARPROC __stdcall hkGetProcAddress(HMODULE hModule, LPCSTR lpProcName);
 
-bool InitHooks();
+bool InitHooks(void);
