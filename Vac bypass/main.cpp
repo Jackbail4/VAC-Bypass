349986016f19f7443af9c7d1f0e63b18c44e49ca
Jackbail4 <64487175+Jackbail4@users.noreply.github.com>
2022-01-30 13:03:45 +0000

    Update main.cpp

--- Vac bypass/main.cpp
+++ Vac bypass/main.cpp
@@ -1,20 +1,16 @@
-#include <Windows.h>
-
-extern bool InitHooks();
+#include "Hooks.h"
 
 void MainThread(HMODULE hModule) {
-	if (InitHooks()) {
-		MessageBoxA(0, "Success", "Hooks Successful",MB_OK);
-	}
-	else {
-		MessageBoxA(0, "Error", "No kernel32 module",MB_OK);
-	}
+	if (InitHooks())
+		MessageBoxA(0, "Success", "Vac Bypassed!", MB_OK);
+	else
+		MessageBoxA(0, "Error", "An error occured!", MB_OK);
 
 	FreeLibraryAndExitThread(hModule, 0);
 }
 
 BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPARAM lParam) {
 	if (dwReason == DLL_PROCESS_ATTACH) {
-		CloseHandle(CreateThread(0, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, 0));
+		CloseHandle(CreateThread(nullptr, 0, (LPTHREAD_START_ROUTINE)MainThread, hModule, 0, nullptr));
 	}
 }
