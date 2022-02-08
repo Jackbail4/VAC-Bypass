e1b9694d2ec61eca7e6d9ac11c47f2ccb988192a
Jackbail4 <64487175+Jackbail4@users.noreply.github.com>
2022-02-08 03:01:36 +0000

    Update Dllmain.cpp

--- VAC Bypass 2022/Dllmain.cpp
+++ VAC Bypass 2022/Dllmain.cpp
@@ -11,7 +11,7 @@ void MainThread(HMODULE hModule) {
 		MessageBoxA(0, "Could not find steamservice.dll - Restart steam with admin...", "VAC BYPASS", MB_OK);
 	else {
 		if (InitVACHooks((void*)hSteamService))
-			MessageBoxA(0, "Patched VAC. You may start a CSGO.", "VAC Bypass", MB_OK);
+			MessageBoxA(0, "Patched VAC. You may start CSGO.", "VAC Bypass", MB_OK);
 		else
 			MessageBoxA(0, "An error has occured. Please close steam...", "VAC Bypass", MB_OK);
 	}
